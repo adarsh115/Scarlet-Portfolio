d0c119b3a006c6c9ff82a956d2ab013199ac5892
Adarsh <44157141+adarsh115@users.noreply.github.com>
2022-09-26 22:15:07 +0530

    responsive main nav bar complete

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const hamburgerContainer = document.querySelector("#header__main-nav");
+
 const hamburger = document.querySelector(".header__main-nav--hamburger");
+
 const links = document.querySelectorAll(".header__main-nav--links li");
 
-hamburger.addEventListener("click", () => {
-  hamburgerContainer.classList.toggle("clicked");
-  links.forEach((link) => {
-    link.classList.toggle("fade");
-  });
+hamburger.addEventListener('click', () => {
+    hamburgerContainer.classList.toggle('clicked');
+    links.forEach((link) => {
+        link.classList.toggle('fade')
+    });
 });
